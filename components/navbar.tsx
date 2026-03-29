194e8524131b091488b17d137db507ef255089ad
AbhishekSunar01 <a8hishek01@gmail.com>
2026-03-29 00:59:18 +0000

    Navbar done

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,21 +1,34 @@
+import { User } from "lucide-react"
+import { Croissant } from "lucide-react"
+import Link from "next/link"
+
 export default function Navbar() {
   return (
     <nav className="border-b px-72 py-4">
       <div className="container mx-auto flex items-center justify-between">
-        <div className="text-lg font-bold">Food Ordering App</div>
-        <div>
-          <a href="/" className="rounded px-3 py-2">
+        <div className="flex items-center gap-2 text-3xl font-bold">
+          <div className="text-accent">
+            <Croissant width={40} height={40} />
+          </div>
+          Food Ordering App
+        </div>
+        <div className="flex items-center gap-4">
+          <Link href="/" className="rounded px-3 py-2">
             Home
-          </a>
-          <a href="/menu" className="rounded px-3 py-2">
+          </Link>
+          <Link href="/" className="rounded px-3 py-2">
             Menu
-          </a>
-          <a href="/orders" className="rounded px-3 py-2">
+          </Link>
+          <Link href="/" className="rounded px-3 py-2">
             Orders
-          </a>
-          <a href="/contact" className="rounded px-3 py-2">
-            Contact
-          </a>
+          </Link>
+          <Link
+            href="/"
+            className="flex items-center gap-2 rounded-full bg-secondary px-3 py-2"
+          >
+            <User className="h-5 w-5" />
+            Abhishek
+          </Link>
         </div>
       </div>
     </nav>
