b6082c993f9d93eb877e1bd553cb2895f105b78c
nityashahi1 <thakurinitu263@gmail.com>
2026-03-29 02:00:26 +0100

    added product card

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,4 +1,4 @@
-export default function Navbar() {
+  export default function Navbar() {
   return (
     <nav className="border-b px-72 py-4">
       <div className="container mx-auto flex items-center justify-between">
